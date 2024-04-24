3b8c722ca558f5d8451a0dbf333bb1d59a4c073a
Ujwal Ghate <ujwalghate@Ujwals-MacBook-Pro.local>
2024-04-24 18:22:55 +0530

    Add total Amount & some css Part

--- src/products.js
+++ src/products.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 const Products = () => {
     const [products, setProduct]= useState([]);
     const [count, setCount]=useState(0);
+    const [amount, setAmount]=useState(0);
     const [cartItem, setcartItem]=useState([]);
     useEffect(()=>{
         fetch("https://fakestoreapi.com/products")
@@ -14,6 +15,7 @@ const Products = () => {
     const addCart=(prod) =>{
 
         setCount(count+1);
+        setAmount(amount+prod.price)
         const data = products.filter((pro)=>pro.id === prod.id)
         
         setcartItem(data);
@@ -23,9 +25,11 @@ const Products = () => {
         <header className='header'>
             <img src="https://st2.depositphotos.com/4035913/6124/i/450/depositphotos_61243733-stock-illustration-business-company-logo.jpg" alt='logo' />
             <p>Cart: {count}</p>
+            <p>Total Amount: {amount}</p>
             <p>CartItems: {cartItem.map((prod)=>(
                 <li key={prod.id}>
                     {prod.title}
+                    <img style={{height:'30px', width:'30px'}} src={prod.image} alt={prod.title}/>
                 </li>
             ))}</p>
 
@@ -37,7 +41,7 @@ const Products = () => {
             <li key={index}>
                 <div className='card'>
                 <img src={prod.image} alt={prod.title}/>
-                <h2>{prod.title}</h2>
+                <h2 style={{textAlign: 'center'}}>{prod.title}</h2>
                 <p>Price: {prod.price}</p>
                 <p>Rating: {prod.rating.rate}</p>
                 <button onClick={()=>addCart(prod)}>Buy</button>
